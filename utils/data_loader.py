11087170a10433cc0cbf07b3850c1809e0da97eb
YS-Kutuzova <ys.ksenia@gmail.com>
2025-10-30 19:56:36 +0300

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -11,28 +11,38 @@ API_URL = os.getenv("API_URL")
 def load_data(city):
     response = requests.get(API_URL, params={'key': API_KEY, 'q': city, 'days': 1, 'aqi': 'yes'})
     data = response.json()
+
+    # Оставляем основную информацию по погодным условиям, без прогнозов и графиков
     city_name = data['location']['name']
     temp = data['current']['temp_c']
     condition = data['current']['condition']['text']
     icon = data['current']['condition']['icon']
+    wind_speed = data['current']['wind_kph'] / 3.6
 
+    # Качество воздуха по часам
     forecast_hours = data['forecast']['forecastday'][0]['hour']
     hours = [hour['time'][-5:] for hour in forecast_hours]
-    temps = [hour['temp_c'] for hour in forecast_hours]
-    ap = [hour['pressure_mb'] * 0.75 for hour in forecast_hours]
-    wind = [hour['wind_kph'] / 3.6 for hour in forecast_hours]
-    wind_dirs = [hour['wind_degree'] for hour in forecast_hours]
-    air_quality = data['current']['air_quality']
+
+    pm25_hours = [hour['air_quality']['pm2_5'] for hour in forecast_hours]
+    pm10_hours = [hour['air_quality']['pm10'] for hour in forecast_hours]
+    co_hours = [hour['air_quality']['co'] for hour in forecast_hours]
+    no2_hours = [hour['air_quality']['no2'] for hour in forecast_hours]
+    o3_hours = [hour['air_quality']['o3'] for hour in forecast_hours]
+    so2_hours = [hour['air_quality']['so2'] for hour in forecast_hours]
 
     return {
         'city_name': city_name,
         'temp': temp,
         'condition': condition,
         'icon': icon,
-        'temps': temps,
-        'ap': ap,
-        'wind': wind,
-        'wind_dirs': wind_dirs,
+        'wind_speed': round(wind_speed, 1),
+
         'hours': hours,
-        'air_quality': air_quality #для задания
+        'pm25_hours': pm25_hours,
+        'pm10_hours': pm10_hours,
+        'co_hours': co_hours,
+        'no2_hours': no2_hours,
+        'o3_hours': o3_hours,
+        'so2_hours': so2_hours
+
     }
